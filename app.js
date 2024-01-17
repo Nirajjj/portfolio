4739d9ef8862c2a5c97ff940320877a0d7e11942
Niraj Parab <nirajparab007@gmail.com>
2024-01-17 16:06:55 +0530

    connect all heading links to respective content of webpage and connect form to google sheet

--- app.js
+++ app.js
@@ -32,3 +32,33 @@ function showTab(para) {
 //   })
   
 // })
+
+let menuIcon = document.querySelector('.fa-bars');
+let menuCloseIcon = document.querySelector('.fa-xmark');
+
+menuIcon.addEventListener('click', ()=> {
+  let menu = document.querySelector('nav ul');
+  menu.style.right = '0';
+  
+})
+
+menuCloseIcon.addEventListener('click', ()=> {
+  let menu = document.querySelector('nav ul');
+  menu.style.right = '-190px';
+})
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyyeyHg7ASxRmonO2MtjvAO2UP3vUPVwPT7WtCPtg9Q1JaPVI7P7IegC-9GFJ99cXuA/exec'
+  const form = document.forms['submit-to-google-sheet']
+  let msg = document.querySelector('#msg');
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+      msg.innerHTML = 'message sent successfully🚀'
+      form.reset()
+      setTimeout(()=> {
+        msg.innerHTML = '';
+      }, 5000)
+  })
